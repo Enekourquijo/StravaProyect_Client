a422d42130cd61ad77dbe824596bdac634562dfa
Enekourquijo <eneko.urquijo@opendeusto.es>
2022-11-18 11:49:48 +0100

    rstyjmt
    Co-Authored-By: ismaelalcaide <91288325+ismaelalcaide@users.noreply.github.com>

---
+++ src/ClienteController/cacsa.java
@@ -0,0 +1,6 @@
+package ClienteController;
+
+public class cacsa {
+ //aqui va todo las funciones y metoddos al los que vas a llamr en 
+	//los listener de las ventanas(mirar ejemplo y cambiar metodos
+}

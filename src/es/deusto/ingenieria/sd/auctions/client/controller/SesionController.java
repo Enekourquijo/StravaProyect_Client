26090b56551046f87026e27dc4642ae25d86e2d5
Enekourquijo <eneko.urquijo@opendeusto.es>
2022-11-19 13:23:25 +0100

    reto y service creados
    Co-Authored-By: ismaelalcaide <91288325+ismaelalcaide@users.noreply.github.com>
    Co-Authored-By: JaimeRevilla <90686026+JaimeRevilla@users.noreply.github.com>
    Co-Authored-By: enriquepuente2 <91028770+enriquepuente2@users.noreply.github.com>

---
+++ src/es/deusto/ingenieria/sd/auctions/client/controller/SesionController.java
@@ -0,0 +1,32 @@
+package es.deusto.ingenieria.sd.auctions.client.controller;
+
+import java.util.List;
+
+import es.deusto.ingenieria.sd.auctions.client.remote.ServiceLocator;
+
+public class SesionController {
+	//Reference to the Service Locator
+    private ServiceLocator serviceLocator;
+
+    public SesionController(ServiceLocator serviceLocator) {
+        this.serviceLocator = serviceLocator;
+    }
+
+    public List<String> getDeporte() {
+        try {
+            return this.serviceLocator.getService().getDeporte();
+        } catch (Exception e) {
+            System.out.println(" # Error during getDep: " + e);
+            return null;
+        }
+    }
+
+    public void makeSesion(String titulo, String deporte, double km, String fInicio, int hora, double duracion) {
+        try {
+            this.serviceLocator.getService().makeSesion(titulo, deporte, km, fInicio, hora, duracion);
+        } catch(Exception e) {
+            System.out.println("# Error during session making: " + e);
+        }
+    }
+
+}
